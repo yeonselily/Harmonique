e206f7be8a27785304512b35ffb11708e1718511
Seyeon Park <96575761+yeonselily@users.noreply.github.com>
2026-01-25 19:00:55 -0800

    changed the grid from lg:grid-cols-3 with lg:col-span-2 on the first box to lg:grid-cols-2, so now both the "Detected Emotions" and "AI Emotion Detection" boxes are equally divided

--- audio-emotion-scapes-25-main/src/pages/Index.tsx
+++ audio-emotion-scapes-25-main/src/pages/Index.tsx
@@ -216,8 +216,8 @@ const Index = () => {
               </p>
             </div>
             
-            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-              <div className="lg:col-span-2">
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+              <div>
                 <MoodSelector 
                   selectedMood={selectedMood}
                   onMoodSelect={handleMoodSelect}
